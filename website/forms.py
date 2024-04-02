6b946ed7a95e8326c9c39d133bb30f6593a772f9
MuhammadSalmon <saidovsomon123@gmail.com>
2024-04-02 22:38:13 +0500

    finish

--- website/forms.py
+++ website/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
+
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={"class": "mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md", 'placeholder': 'Email'}))
@@ -11,22 +13,44 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-        
-        def __init__(self, *args, **kwargs):
-            super(SignUpForm, self).__init__(*args, **kwargs)
 
-            self.fields['username'].widget.attrs['class'] = 'form-input'
-            self.fields['username'].widget.attrs['placeholder'] = 'User Name'
-            self.fields['username'].label = ''
-            self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.</small></span>'
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
 
-            self.fields['password1'].widget.attrs['class'] = 'form-input'
-            self.fields['password1'].widget.attrs['placeholder'] = 'Password'
-            self.fields['password1'].label = ''
-            self.fields['password1'].help_text = '<ul class="text-sm text-gray-500"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
+        # Update the class and placeholder attributes for the username field
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-input',
+            'placeholder': 'User Name'
+        })
 
-            self.fields['password2'].widget.attrs['class'] = 'form-input'
-            self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
-            self.fields['password2'].label = ''
-            self.fields['password2'].help_text = '<span class="text-sm text-gray-500"><small>Enter the same password as before, for verification.</small></span>'	
+        # Update the class and placeholder attributes for the password1 field
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-input',
+            'placeholder': 'Password'
+        })
 
+        # Update the class and placeholder attributes for the password2 field
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-input',
+            'placeholder': 'Confirm Password'
+        })
+
+        # Update help text for the password1 and password2 fields
+        self.fields['password1'].help_text = '<br> <br><ul class="text-sm text-gray-500"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul> <br>'
+        self.fields['password2'].help_text = '<br> <span class="text-sm text-gray-500"><small>Enter the same password as before, for verification.</small></span>'
+        self.fields['username'].help_text = ''
+
+
+class AddRecordForm(forms.ModelForm):
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    phone = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+    zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 p-2 rounded-md"}), label="")
+
+    class Meta:
+        model = Record
+        exclude = ("user",)
